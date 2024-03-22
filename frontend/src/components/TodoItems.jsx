53f6e6321f432d3ddcc7788a0759ed19d8a5cba1
Michael Lee <kymiclee@gmail.com>
2024-03-22 19:53:27 +0000

    Creating base Css for the website

---
+++ frontend/src/components/TodoItems.jsx
@@ -0,0 +1,13 @@
+export default function TodoItems() {
+    return (
+        <div>
+            <h3>TodoItem 1</h3>
+            <h3>TodoItem 2</h3>
+            <h3>TodoItem 3</h3>
+            <h3>TodoItem 4</h3>
+            <h3>TodoItem 5</h3>
+            <h3>TodoItem 6</h3>
+        </div>
+
+    )
+}
---
+++ frontend/src/components/TodoLists.jsx
@@ -0,0 +1,13 @@
+export default function TodoList() {
+    return (
+        <div>
+            <h3>TodoList 1</h3>
+            <h3>TodoList 2</h3>
+            <h3>TodoList 3</h3>
+            <h3>TodoList 4</h3>
+            <h3>TodoList 5</h3>
+            <h3>TodoList 6</h3>
+        </div>
+
+    )
+}
